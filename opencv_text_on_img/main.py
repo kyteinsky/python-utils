7e2a16b13c903790cf9426f943fd4d312744eaf3
kyte <kyteinsky@gmail.com>
2020-09-27 00:09:33 +0530

    enhanced features for opencv-txt

--- opencv_text_on_img/main.py
+++ opencv_text_on_img/main.py
@@ -9,9 +9,8 @@ class OpencvText():
         self.output_path = output_path
     
 
-    def write_text(self, img_name, text): # returns (extension of image, modified image)
-        img = cv2.imread(img_name, cv2.COLOR_BGR2RGB)
-        if img is None: raise 'Not an image or not in correct format!'
+    def write_text(self, img, text): # returns (extension of image, modified image)
+        
         thickness = 2
         ratio_sin = 1
 
@@ -35,8 +34,13 @@ class OpencvText():
         box_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width + int(15 * ratio_sin), text_offset_y - text_height - int(15 * ratio_sin)))
         cv2.rectangle(img, box_coords[0], box_coords[1], rectangle_bgr, cv2.FILLED)
         cv2.putText(img, text, (text_offset_x, text_offset_y-int(10 * ratio_sin)), cv2.FONT_HERSHEY_COMPLEX, fontScale=font_scale, color=(51, 153, 255), thickness=thickness)
-        return os.path.splitext(img_name)[1], img
+        return img
         # cv2.imwrite(os.path.join(self.folder, self.output_path, text)[:-1]+os.path.splitext(img_name)[1], img)
+
+    def read_img(self, image_path):
+        img = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
+        if img is None: raise 'Not an image or not in correct format!'
+        return img
 
     def image_filenames(self, folder): # list all valid images
         img_files = []
@@ -51,22 +55,30 @@ class OpencvText():
         for im in self.image_filenames(os.path.join(self.folder, self.input_path)):
             if text_array != None:
                 for txt in text_array:
-                    ext, img = self.write_text(im, txt)
-                    cv2.imwrite(os.path.join(self.folder, self.output_path, text)[:-1]+ext, img)
+                    img = self.read_img(im)
+                    img = self.write_text(img, txt)
+                    cv2.imwrite(os.path.join(self.folder, self.output_path, text)[:-1]+os.path.splitext(im)[1], img)
             else:
                 i += 1
                 text = f'{i}hello this is just a test for checking out opencv write on image!'
-                ext, img = self.write_text(im, text)
-                cv2.imwrite(os.path.join(self.folder, self.output_path, text)[:-1]+ext, img)
+                img = self.read_img(im)
+                img = self.write_text(img, text)
+                cv2.imwrite(os.path.join(self.folder, self.output_path, text)[:-1]+os.path.splitext(im)[1], img)
 
-    def run_image(self, image, text):
-        if image != None and text != None:
-            ext, img = self.write_text(image, text)
-            cv2.imwrite(os.path.join(os.path.dirname(image), text)+ext, img)
+    def run_image(self, image_name, text):
+        if image_name != None and text != None:
+            img = self.read_img(image_name)
+            img = self.write_text(img, text)
+            cv2.imwrite(os.path.join(os.path.dirname(image_name), text)+os.path.splitext(image_name)[1], img)
         else:
-            raise 'Input image and text'
+            raise 'Input image path and text to write'
+
+    def run_numpy_image(self, image, text, output_dir='./', ext='jpg'):
+        img = self.write_text(image, text)
+        cv2.imwrite(os.path.join(output_dir, text)+f'.{ext}', img)
 
 
 # op = OpencvText()
 # op.run_folder()
 # op.run_image('input/6t8Zh249QiFmVnkQdCCtHK.jpg', 'test image')
+# op.run_numpy_image(op.read_img('input/6t8Zh249QiFmVnkQdCCtHK.jpg'), 'numpy test on sample image', output_dir='input', ext='png')
